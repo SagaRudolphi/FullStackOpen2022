69b1ec5c5fcabfb49382c4c199472f7135fc4610
Saga Rudolphi <Saga.Rudolphi@sigma.se>
2022-04-07 16:11:21 +0200

    1.9: unicafe step 4

--- Part1/unicafe/src/App.js
+++ Part1/unicafe/src/App.js
@@ -1,17 +1,24 @@
 import {useState} from 'react'
 
 const Statistics = (props) => {
-  return (
-    <div>
-      <h1>Statistics</h1>
-      <p>Good: {props.good}</p>
-      <p>Neutral: {props.neutral}</p>
-      <p>Bad: {props.bad}</p>
-      <p>All: {props.all}</p> 
-      <p>Avarage: {(props.good - props.bad)/props.all}</p> 
-      <p>Percentage: {(props.good / props.all) * 100} % </p>
-    </div>
-  )
+
+  if (props.allVotes === 0){
+    return (
+      <div> 
+          <p>No feedback given</p> 
+      </div>
+    )
+  }
+    return (
+      <div>
+        <p>Good: {props.good}</p>
+        <p>Neutral: {props.neutral}</p>
+        <p>Bad: {props.bad}</p>
+        <p>All: {props.allVotes}</p> 
+        <p>Avarage: {(props.good - props.bad)/props.allVotes}</p> 
+        <p>Percentage: {(props.good / props.allVotes) * 100} % </p>
+      </div>
+    )
 }
 
 const App = () => {
@@ -19,27 +26,27 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(0)
+  const [allVotes, setAll] = useState(0)
  
 
   const handleGoodClicks = () => {
     setGood(good + 1)
-    setAll(all + 1)
+    setAll(allVotes + 1)
   }
   
   const handleNeutralClicks = () => {
     setNeutral(neutral + 1)
-    setAll(all + 1)
+    setAll(allVotes + 1)
   }
 
   const handleBadClicks = () => {
     setBad(bad + 1)
-    setAll(all + 1)
+    setAll(allVotes + 1)
   }
 
 
   return (
-    console.log('Good: ', good, ' Neutral: ', neutral, ' Bad: ', bad, ' All: ', all),
+    console.log('Good: ', good, ' Neutral: ', neutral, ' Bad: ', bad, ' All: ', allVotes),
     <div>
       <div>
       <h1>Give feedback</h1>
@@ -47,7 +54,8 @@ const App = () => {
       <button onClick={handleNeutralClicks}>Neutral</button>
       <button onClick={handleBadClicks}>Bad</button>
     </div>
-    <Statistics bad = {bad} good = {good} neutral = {neutral} all = {all} />
+    <h1>Statistics</h1>
+    <Statistics bad = {bad} good = {good} neutral = {neutral} allVotes = {allVotes} />
     </div>
     
   );

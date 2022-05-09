dfee3672a14f49083dd88de587c307297728d828
Saga Rudolphi <Saga.Rudolphi@sigma.se>
2022-05-09 19:47:15 +0200

    4.4 helper functions and unit tests, step2

--- Part4/utils/list_helper.js
+++ Part4/utils/list_helper.js
@@ -3,7 +3,19 @@ const blogsRouter = require('../controllers/blogs')
 const dummy = (blogs) => {
     return 1
   }
+
+  const totalLikes = (blogs) => {
+    if (blogs.length === 0) {
+        return null
+    }
+    else{
+    const allLikes = blogs.map(blog => blog['likes'])
+    const totalLikes = allLikes.reduce((previousVal, currentVal) => previousVal + currentVal, 0)
+    return totalLikes}
+  }
+  
   
   module.exports = {
-    dummy
+    dummy,
+    totalLikes
   }

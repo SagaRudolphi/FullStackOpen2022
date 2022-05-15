eb494489950bdf6e43ef85b888fb379eff29f395
Saga Rudolphi <Saga.Rudolphi@sigma.se>
2022-05-15 21:37:03 +0200

    4.13 Blog list expansions, step1

--- Part4/controllers/blogs.js
+++ Part4/controllers/blogs.js
@@ -15,5 +15,14 @@ blogsRouter.post('/', async (request, response) => {
   const newBlog = await blog.save()
   response.status(201).json(newBlog)
 })
+
+blogsRouter.delete('/:id', async (request, response, next) => {
+    try {
+      await Blog.findByIdAndRemove(request.params.id)
+      response.status(204).end()
+    } catch (exception){
+      next(exception)
+    }
+  })
   
   module.exports = blogsRouter
